0ee91e08cd160df9b2b18b9bde2a997cbff48083
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-11-12 16:34:33 +0100

    Po dodání třídy Find.

---
+++ src/main/java/cz/domaciukollekce10/Find.java
@@ -0,0 +1,11 @@
+package cz.domaciukollekce10;
+
+import java.util.List;
+
+public class Find {
+
+    public static Double findMatching(List<Double> list, Double lowerLimit, Double upperLimit) {
+        return list.get(0);
+    }
+
+}
