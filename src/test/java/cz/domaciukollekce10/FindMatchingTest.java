ff11b5b479f7feb852a9885c29c4eb05b130e83f
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-11-12 20:40:19 +0100

    Tak snad OK.

--- src/test/java/cz/domaciukollekce10/FindMatchingTest.java
+++ src/test/java/cz/domaciukollekce10/FindMatchingTest.java
@@ -1,20 +1,23 @@
-package cz.domaciukollekce10;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
+import cz.domaciukollekce10.Find;
+import org.junit.jupiter.api.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FindMatchingTest {
 
     private Find findNumber;
+    private List<Double> testList;
+
+    @BeforeEach
+    public void setUp() {
+        testList = new ArrayList<>();
+    }
 
     @Test
     @DisplayName("Zero task - exercise")
     public void middle1Test () {
-        List<Double> testList = List.of(10.0, 15.45, 5.811, 175.0);
+        testList = List.of(10.0, 15.45, 5.811, 175.0);
         Double testResult = Find.findMatchingNumber(testList, 4.0, 9.3);
         Assertions.assertEquals(5.811, testResult);
     }
@@ -22,7 +25,6 @@ public class FindMatchingTest {
     @Test
     @DisplayName("Exception test")
     public void emptyTest () {
-        List<Double> testList = new ArrayList<>();
         Assertions.assertThrows(Exception.class, () -> Find.findMatchingNumber
                 (testList, 10.0, 12.0));
     }
@@ -30,22 +32,42 @@ public class FindMatchingTest {
     @Test
     @DisplayName("First self-test according homework")
     public void middle2Test() {
-        List<Double> testList = List.of(12.0, 8.126, 6.0);
+        testList = List.of(12.0, 8.126, 6.0);
         Double testResult = Find.findMatchingNumber(testList, 11.0, 12.6);
         Assertions.assertEquals(12.0, testResult);
     }
 
     @Test
-    @DisplayName("Second self-test according homework - Lamda")
+    @DisplayName("Second self-test according homework - Lambda")
     public void middle3Test() {
-        List<Double> testList = List.of(12.0, 8.126, 9.0, 158.0, 6.0);
-        Assertions.assertDoesNotThrow(
-                // pokus o zápis dle Lambda zápisu - tak snad...
-                () -> {
-                    Double testResult = Find.findMatchingNumber(testList, 8.0, 10.0);
+        testList = List.of(12.0, 8.126, 9.0, 158.0, 6.0);
+        Assertions.assertDoesNotThrow(() -> {
+            Double testResult = Find.findMatchingNumber(testList, 8.0, 10.0);
             Assertions.assertEquals(8.126, testResult);
-                }
-        );
+        });
+    }
+
+    // Tak toto bylo logický peklo! A vůbec si nejsem jist, že tento kód je v pořádku...
+    @RepeatedTest(100)
+    @DisplayName("Random test")
+    public void randomTest() {
+        Random random = new Random();
+        testList.clear();
+        int size = random.nextInt(10) + 1;
+        for (int i = 0; i < size; i++) {
+            testList.add(random.nextDouble() * 100);
+        }
+        double lowerLimit = random.nextDouble() * 100;
+        double upperLimit = lowerLimit + random.nextDouble() * (100 - lowerLimit); // tady to mám určitě OK
+        try {
+            Double testResult = findNumber.findMatchingNumber(testList, lowerLimit, upperLimit);
+            Assertions.assertTrue(testList.contains(testResult) && testResult >= lowerLimit
+                            && testResult <= upperLimit,
+                    "Výsledek testu NENÍ v mezích rozsdahu¨!");
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals("Žádné vhodné číslo nebylo nalezeno...", e.getMessage());
+        }
+
     }
 
 
