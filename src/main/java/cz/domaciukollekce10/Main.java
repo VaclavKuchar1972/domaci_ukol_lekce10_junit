88965a52769838531ae0cffe0f24d1fe3189c5f7
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-11-12 19:16:50 +0100

    Tak asi OK.

--- src/main/java/cz/domaciukollekce10/Main.java
+++ src/main/java/cz/domaciukollekce10/Main.java
@@ -3,17 +3,6 @@ package cz.domaciukollekce10;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
 
-        //System.out.println("Domácí úkol - Lekce 10 - JUnit");
-
-        //Find find = new Find();
-
-        //Double firstMatchingNumber = find.findMatchingNumber
-        //        (Arrays.asList(1.0, 2.0, 3.0, 4.0), 4.0, 9.3);
-
-        //System.out.println(firstMatchingNumber);
-
-
-    }
 }
